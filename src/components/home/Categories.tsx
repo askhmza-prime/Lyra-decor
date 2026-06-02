7f68b9c0acaefe960c5621f32b1cf39e37e68bf4
askhmza-prime <askhmza@gmail.com>
2026-06-02 10:55:17 +0530

    Create Categories component for displaying categories

---
+++ src/components/home/Categories.tsx
@@ -0,0 +1,9 @@
+export default function Categories() {
+  return (
+    <section className="py-20 text-center">
+      <h2 className="text-4xl font-display">
+        Categories
+      </h2>
+    </section>
+  )
+}
