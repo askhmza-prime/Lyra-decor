2a150b650a9a87760efe30239dbc642dc549e710
askhmza-prime <askhmza@gmail.com>
2026-06-02 10:30:12 +0530

    Add Navbar, Hero, and Footer components to HomePage

--- src/(public)/page.tsx
+++ src/(public)/page.tsx
@@ -1,1 +1,13 @@
+import Navbar from '@/components/layout/Navbar'
+import Footer from '@/components/layout/Footer'
+import Hero from '@/components/home/Hero'
 
+export default function HomePage() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </>
+  )
+}
