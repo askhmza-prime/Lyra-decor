6cfb1b42eccc6341482471e8fbf3dbec677cbb28
askhmza-prime <askhmza@gmail.com>
2026-06-02 10:55:42 +0530

    Add WhyChooseUs component for homepage

---
+++ src/components/home/WhyChooseUs.tsx
@@ -0,0 +1,9 @@
+export default function WhyChooseUs() {
+  return (
+    <section className="py-20 text-center">
+      <h2 className="text-4xl font-display">
+        Why Choose Lyra
+      </h2>
+    </section>
+  )
+}
