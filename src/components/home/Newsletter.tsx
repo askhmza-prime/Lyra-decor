fb034635c9e8f142dd4cade37e4cc494a65a3978
askhmza-prime <askhmza@gmail.com>
2026-06-02 10:56:36 +0530

    Add Newsletter component for subscription section

---
+++ src/components/home/Newsletter.tsx
@@ -0,0 +1,9 @@
+export default function Newsletter() {
+  return (
+    <section className="py-20 text-center">
+      <h2 className="text-4xl font-display">
+        Join Our Newsletter
+      </h2>
+    </section>
+  )
+}
