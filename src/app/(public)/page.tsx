7f6e0bf325d05f4846c319862d5bc91a6b3ea02b
askhmza-prime <askhmza@gmail.com>
2026-06-02 10:46:00 +0530

    Refactor HomePage layout to include new components

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,13 +1,17 @@
-import Navbar from '@/components/layout/Navbar'
-import Footer from '@/components/layout/Footer'
 import Hero from '@/components/home/Hero'
+import FeaturedProducts from '@/components/home/FeaturedProducts'
+import Categories from '@/components/home/Categories'
+import WhyChooseUs from '@/components/home/WhyChooseUs'
+import Newsletter from '@/components/home/Newsletter'
 
 export default function HomePage() {
   return (
-    <>
-      <Navbar />
+    <main>
       <Hero />
-      <Footer />
-    </>
+      <FeaturedProducts />
+      <Categories />
+      <WhyChooseUs />
+      <Newsletter />
+    </main>
   )
 }
